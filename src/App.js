c094d1cda13c86f8635b410fdc834e0bcfbfbc36
Chanchal Kumari <solankichanchal658@gmail.com>
2024-03-22 17:04:55 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import './App.css';
 import About from './components/About';
 import BannerSection from './components/BannerSection';
